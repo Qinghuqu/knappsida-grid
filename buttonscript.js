47a8d04120f19d65d5feb40bef2aa9dde18aca70
qinghuqu <qinghuqu@gmail.com>
2021-09-23 09:46:34 +0200

    Add dimmer

--- buttonscript.js
+++ buttonscript.js
@@ -3,10 +3,12 @@ const alert = document.getElementById("container");
 const ok = document.getElementById("ok");
 const cancel = document.getElementById("cancel");
 const xbutton = document.getElementById("X");
+const dimmer = document.getElementById("dimmer");
 
 delbutton.addEventListener("click", function(){
   delbutton.style="display: none";
   alert.style="display: grid";
+  dimmer.style="display: block"
 });
 ok.addEventListener("click", function(){
   window.open("http://hmpg.net", "_self");
@@ -14,8 +16,10 @@ ok.addEventListener("click", function(){
 cancel.addEventListener("click", function(){
   delbutton.style="display: block";
   alert.style="display: none";
+  dimmer.style="display: none";
 });
 xbutton.addEventListener("click", function(){
   delbutton.style="display: block";
   alert.style="display: none";
+  dimmer.style="display: none";
 });
